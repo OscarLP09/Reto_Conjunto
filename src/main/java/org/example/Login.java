b1e75646fa4846871f546333898646031a765ec5
Óscar Luque Porca <oscarluqueproca@gmail.com>
2024-10-16 17:51:04 +0200

    Reto Conjunto DI y AD

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Login {
+    private JButton loginBtn;
+    private JButton exitBtn;
+    private JTextField userTxt;
+    private JPasswordField passwordTxt;
+}
