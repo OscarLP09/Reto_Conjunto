4251613596bc89f2d389a38e942f23c5641b1627
Óscar Luque Porca <oscarluqueproca@gmail.com>
2024-10-16 21:07:35 +0200

    Reto Conjunto DI y AD

--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -2,9 +2,11 @@ package org.example;
 
 import javax.swing.*;
 
-public class Login {
+public class Login extends JFrame {
     private JButton loginBtn;
     private JButton exitBtn;
     private JTextField userTxt;
     private JPasswordField passwordTxt;
+
+
 }
